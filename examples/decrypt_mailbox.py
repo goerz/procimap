de082609edc748fbfed0c7653b1caddf5257cd0f
Michael Goerz <goerz@physik.fu-berlin.de>
2008-09-04 17:33:46 +0000

    Updated decrypt_mailbox script. This is what I use for gmail

--- examples/decrypt_mailbox.py
+++ examples/decrypt_mailbox.py
@@ -6,27 +6,30 @@ import sys
 
 from ProcImap.Utils import pipe_message
 from ProcImap.MailboxFactory import MailboxFactory
+from ProcImap.ImapMailbox import ImapMailbox
 
 
 mailboxes = MailboxFactory('/home/goerz/.procimap/mailboxes.cfg')
-encr_boxes = [mailboxes["Physik"], mailboxes["PhysikSent"]]
-backupmailbox = mailboxes.get("Backup")
+encr_boxes = [ImapMailbox((mailboxes.get_server("Gmail"), 'INBOX')), 
+              ImapMailbox((mailboxes.get_server("Gmail"), '[Gmail]/Sent Mail'))
+             ]
 
 decryptprogram = "/home/goerz/bin/eml_decrypt.pl -mbox -w"
 
 
 for mailbox in encr_boxes:
-    print "Processing mailbox %s" % mailbox.name
+    mailbox.trash = '[Gmail]/Trash' # for backup (and obligatory for gmail)
+    print "\n\nProcessing mailbox %s" % mailbox.name
     encrypted = mailbox.search('UNDELETED HEADER Content-Type encrypted')
     for uid in encrypted:
         print "    Decrypting UID %s" % uid
-        mailbox.copy(uid, backupmailbox)
         message = mailbox[uid]
+        print "        Deleting the original (encrypted) message"
         mailbox.discard(uid)
+        print "        Piping message through decryption program"
         message = pipe_message(message, decryptprogram)
+        print "        Putting decrypted text back into original mailbox %s" \
+              % mailbox.name
         mailbox.add(message)
     mailbox.close()
 sys.exit(0)
-
-
-
