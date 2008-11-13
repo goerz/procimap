c478b90a13d5c749368cb669c8c7f82678933b2d
Michael Goerz <goerz@physik.fu-berlin.de>
2008-11-13 01:04:45 +0000

    Repaired building/distribution files, labeled them as 2.0 (which isn't ready, of course)

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='ProcImap',
-      version='1.1',
+      version='2.0',
       description='Python IMAP Library and Mail Processing Utility',
       author='Michael Goerz',
       author_email='goerz@physik.fu-berlin.de',
       url='http://code.google.com/p/procimap/',
       license='GPL',
-      packages=['ProcImap']
+      packages=['ProcImap', 'ProcImap.Utils']
      )
