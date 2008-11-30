e122aa10cfcb8655cf864295521881052d8e5a10
Michael Goerz <goerz@physik.fu-berlin.de>
2008-11-30 14:25:56 +0000

    Changed decrypt_mailbox example to use CLI util. This is intended for non-Gmail accounts at this point

--- examples/decrypt_mailbox.py
+++ examples/decrypt_mailbox.py
@@ -4,21 +4,26 @@ This example shows how to decrypt a mailbox that has GPG-encrypted emails.
 """
 import sys
 
-from ProcImap.Utils import pipe_message
-from ProcImap.MailboxFactory import MailboxFactory
+from ProcImap.Utils.Processing import pipe_message
+from ProcImap.Utils.CLI import ProcImapOptParser
 from ProcImap.ImapMailbox import ImapMailbox
 
+opt = ProcImapOptParser()
+opt.add_option('--backup', dest='backup', help='backup mailbox')
+(options, args) = opt.parse_args(args=sys.argv)
 
-mailboxes = MailboxFactory('/home/goerz/.procimap/mailboxes.cfg')
-encr_boxes = [ImapMailbox((mailboxes.get_server("Gmail"), 'INBOX')), 
-              ImapMailbox((mailboxes.get_server("Gmail"), '[Gmail]/Sent Mail'))
-             ]
+mainbox = args.pop(0)
+if options.backup is not None:
+    mainbox.trash = options.profile[options.backup]
+encr_boxes = [opt.profile[mainbox]]
+for mb in args:
+    encr_boxes.append(ImapMailbox((opt.profile.get_server(mainbox), mb)))
 
 decryptprogram = "/home/goerz/bin/eml_decrypt.pl -mbox -w"
 
 
 for mailbox in encr_boxes:
-    mailbox.trash = '[Gmail]/Trash' # for backup (and obligatory for gmail)
+    mailbox.trash = encr_boxes[0].trash
     print "\n\nProcessing mailbox %s" % mailbox.name
     encrypted = mailbox.search('UNDELETED HEADER Content-Type encrypted')
     for uid in encrypted:
